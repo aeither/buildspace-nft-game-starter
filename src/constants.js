6b34fe55c0a2dc3c589811d5f5f85561ea258f99
aeither <36173828+aeither@users.noreply.github.com>
2021-10-25 00:21:15 +0200

    .

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = "0x64fAd846EeE0B76a79d58cbB7d6E2cfF6ef31Dcc";
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
